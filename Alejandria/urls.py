6122ddf75f01ade480aad7881ead9c927407c508
OscarFernandezBatalla <ofernaba27@alumnes.ub.edu>
2020-10-23 00:44:35 +0200

    basic main page

--- Alejandria/urls.py
+++ Alejandria/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .view import home
+from . import view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
