43c6990c23953cb34983b1d0872cb35ad4bab79b
ojimenmu <ojimenmu@gmail.com>
2020-10-31 11:14:54 +0100

    #23 #17 #9 FIlters, GET and POST methods advanced, Pending to add some more functions but need to merge first with models

--- books/urls.py
+++ books/urls.py
@@ -6,7 +6,7 @@ app_name = 'books'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('/search/', views.SearchView.as_view(), name='search'),
-    path('<int:pk>/book/', views.BookView.as_view(), name='book'),
+    path('book/<int:pk>/', views.BookView.as_view(), name='book'),
     path('/cart/', views.CartView.as_view(), name='cart'),
     path('/faqs/', views.FaqsView.as_view(), name='faqs'),
 ]
