09bbb4d5a0aca6f4f127cd670ab593a0ca4581a9
OscarFernandezBatalla <ofernaba27@alumnes.ub.edu>
2020-11-19 12:36:24 +0100

    fix thumbnails (no dummies), added photos and database.py adapted

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ import django
 django.setup()
 
 from books.models import Book, User, Address, Product, Cart, FAQ
+from django.core.files import File
 
 import random
 
@@ -12,6 +13,18 @@ user_address = Address(city='Barcelona', street='C/ Test, 112', country='Spain',
 fact_address = Address(city='Barcelona', street='C/ Test, 112', country='Spain', zip='08942')
 user_address.save()
 fact_address.save()
+
+thumb1 = File(open('media/thumbnails/202852714dec217e579db202a977be70.jpg', 'rb'))
+thumb2 = File(open('media/thumbnails/book_cover.jpg', 'rb'))
+thumb3 = File(open('media/thumbnails/book_cover2.jpg', 'rb'))
+thumb4 = File(open('media/thumbnails/book_cover3.jpg', 'rb'))
+thumb5 = File(open('media/thumbnails/book_cover4.jpg', 'rb'))
+thumb6 = File(open('media/thumbnails/book_cover5.jpg', 'rb'))
+thumb7 = File(open('media/thumbnails/book_cover7.jpg', 'rb'))
+thumb8 = File(open('media/thumbnails/canva-white-bold-text-thriller-mystery-book-cover-CejxvxrTCyg.jpg', 'rb'))
+thumb9 = File(open('media/thumbnails/Night_pb-eb-des2.jpg', 'rb'))
+thumb10 = File(open('media/thumbnails/design-for-writers-book-cover-tf-2-a-million-to-one.jpg', 'rb'))
+
 
 
 print("ADDRESS SAVED...OK")
@@ -26,32 +39,32 @@ print("USER SAVED...OK")
 
 book = Book(ISBN="01234565", user_id=user, title="Harry", saga="Potter", description="first harry book",
             author="J.K.Rowling", price=30, language="Spanish", publisher="Alejandria", num_pages=200, num_sold=100,
-            primary_genre="FANT", secondary_genre="OTHR", recommended_age="Juvenil")
+            primary_genre="FANT", secondary_genre="OTHR", recommended_age="Juvenil", thumbnail=thumb10)
 book.save()
 
 book1 = Book(ISBN="012389012", user_id=user, title="Potter", saga="saga", description="first harry book", author="J.ng",
              price=30, language="Spanish", publisher="Alejandria", num_pages=200, num_sold=100, primary_genre="CRIM",
-             secondary_genre="OTHR", recommended_age="Juvenil")
+             secondary_genre="OTHR", recommended_age="Juvenil", thumbnail=thumb3)
 book1.save()
 
 book2 = Book(ISBN="01234567892", user_id=user, title="Sherlock", saga="Harry Potter", description="first harry book",
              author="J.aaa", price=30, language="Spanish", publisher="Alejandria", num_pages=200, num_sold=100,
-             primary_genre="HORR", secondary_genre="OTHR", recommended_age="Juvenil")
+             primary_genre="HORR", secondary_genre="OTHR", recommended_age="Juvenil", thumbnail=thumb9)
 book2.save()
 
 book3 = Book(ISBN="01456789012", user_id=user, title="Holmes", saga="Harry Potter", description="first harry book",
              author="JUse", price=30, language="Spanish", publisher="Alejandria", num_pages=200, num_sold=100,
-             primary_genre="ROMA", secondary_genre="OTHR", recommended_age="Juvenil")
+             primary_genre="ROMA", secondary_genre="OTHR", recommended_age="Juvenil", thumbnail=thumb5)
 book3.save()
 
 book4 = Book(ISBN="01234567852", user_id=user, title="Muerte en el Nilo", saga="Harry Potter",
              description="first harry book", author="Josama", price=30, language="Spanish", publisher="Alejandria",
-             num_pages=200, num_sold=100, primary_genre="FANT", recommended_age="Juvenil")
+             num_pages=200, num_sold=100, primary_genre="FANT", recommended_age="Juvenil", thumbnail=thumb6)
 book4.save()
 
 book5 = Book(ISBN="012349012", user_id=user, title="Hola", saga="Harry Potter", description="first harry book",
              author="Jacinto", price=30, language="Spanish", publisher="Alejandria", num_pages=200, num_sold=100,
-             primary_genre="CRIM", secondary_genre="OTHR", recommended_age="Juvenil")
+             primary_genre="CRIM", secondary_genre="OTHR", recommended_age="Juvenil", thumbnail=thumb7)
 book5.save()
 
 book6 = Book(ISBN="012345812", user_id=user, title="Holita", saga="Harry Potter", description="first harry book",
