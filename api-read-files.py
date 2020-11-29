34dcd5bf57bfa3c18208f7a36a6d9f82e42d8886
ojimenmu <ojimenmu@gmail.com>
2020-11-29 23:33:00 +0100

    API image can't be saved

--- api-read-files.py
+++ api-read-files.py
@@ -1,10 +1,13 @@
 import os
 import traceback
+import urllib
 
 import django
 import json
 import requests
 import time
+import datetime
+from django.core.files import File
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Alejandria.settings")
 django.setup()
@@ -12,14 +15,29 @@ django.setup()
 from books.models import *
 from random import randint
 
+if not os.path.exists('./temp'):
+    os.mkdir('./temp')
+
+
 try:
     adress = Address(street="anonimous", city="Terraplana", country="Illuminados", zip=12345)
     adress.save()
     user = User(role="Admin", name="admin", password="admin", email="admin@mail.com", user_address=adress,
                 fact_address=adress)
     user.save()
+    
+    book = Book(ISBN="123455556345", user_id=user, title="Harry", saga="Potter", description="first harry book",
+                author="J.K.Rowling", price=30, language="Spanish", publisher="Alejandria", num_pages=200, num_sold=100,
+                primary_genre="FANT", secondary_genre="OTHR", recommended_age="Juvenil")
+    thumb10 = File(
+        open('Alejandria/static/images/cover-images/design-for-writers-book-cover-tf-2-a-million-to-one.jpg', 'rb'))
+    book.thumbnail.save('dummy_design.jpg', thumb10) # TODO TESTING
+    book.save()
+    print("Done")
+    
 except:
     user = User.objects.filter(name="admin").first()
+    print("Except")
 
 
 def isbn_10__to__isbn_13(isbn_10):
@@ -38,7 +56,7 @@ def isbn_10__to__isbn_13(isbn_10):
 
 responses = list()
 
-for _, genre in Book.GENRE_CHOICES:
+for _, genre in GENRE_CHOICES:
     print("GENRE: ", genre)
     for word in genre.split(" "):
         params = {'query': '{"type":"\/type\/edition", "genres":' + "\"{}\"".format(word) + '}'}
@@ -62,16 +80,32 @@ for _, genre in Book.GENRE_CHOICES:
                 print('dessc: ', description)
                 saga = json['series'][0] if 'series' in json else None
                 print('saga: ', saga)
-                # publication_date = json["publish_date"] TODO, modify incomming format to YYY-MM-DD or accept multiple formats
+                # publication_date = json["publish_date"]
                 # print('publication date: ', publication_date)
+                publication_date = datetime.time
                 publisher = json["publishers"][0] if "publishers" in json else None
                 print('publisher: ', publisher)
 
-                # authors = json['authors'][0] if 'authors' in json else None #TODO
+                # Author request
+                author_json = json['authors'][0] if 'authors' in json else None
+                if author_json:
+                    print(author_json)
+                    author = requests.get('http://openlibrary.org' + author_json['key'] + '.json')
+                    author = author.json()['name']
+                else:
+                    author = 'Unknown'
 
+                from django.core.files.base import ContentFile
+
+                urllib.request.urlretrieve('https://covers.openlibrary.org/b/isbn/' + isbn + '-M.jpg', './temp/'+isbn+'.jpg')
+                myfile = ContentFile(open('./temp/'+isbn+'.jpg', 'rb').read())
                 book = Book(ISBN=isbn, num_pages=json["number_of_pages"], title=title, description=description,
-                            saga=saga, publication_date=None, primary_genre=_, price=randint(5, 75),
-                            publisher=publisher, user_id=user)
+                            saga=saga, publication_date=publication_date, primary_genre=_, price=randint(5, 75),
+                            publisher=publisher, user_id=user, author=author)
+                path = str(isbn)+'.jpg'
+                print(path)
+                book.thumbnail.save('dummy_design.jpg', myfile)
+                #book.thumbnail.save(path, file_image)
                 print('BOOK: ', book)
                 book.save()
             except:
